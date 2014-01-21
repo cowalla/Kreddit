adf08901a1a7fb70f880596a04a9215d06400f1a
Connor Wallace <wallaconno@gmail.com>
2014-01-20 16:41:40 -0800

    change alignment of 'logged in' status in banner. Begin changes for subreddits

--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -1,6 +1,7 @@
 class Subreddit < ActiveRecord::Base
-  attr_accessible :title, :user_id
+  attr_accessible :title, :user_id, :description
   
   belongs_to :user
   has_many :links
+  
 end
--- db/migrate/20140115182648_create_subreddits.rb
+++ db/migrate/20140115182648_create_subreddits.rb
@@ -2,7 +2,8 @@ class CreateSubreddits < ActiveRecord::Migration
   def change
     create_table :subreddits do |t|
       t.string :title, :null => false
-      t.string :user_id, :null => false
+      t.integer :user_id, :null => false
+      t.text :body
       
       t.timestamps
     end
