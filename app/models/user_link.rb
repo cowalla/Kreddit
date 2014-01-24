34bf3f913a8b4cfdb52cb375c87496c72f2ba323
Connor Wallace <wallaconno@gmail.com>
2014-01-23 18:04:21 -0800

    choose to remove thumbnails from sidebar in favor of simple links

--- app/models/user_link.rb
+++ app/models/user_link.rb
@@ -1,5 +1,5 @@
 class UserLink < ActiveRecord::Base
-  attr_accessible :user_id, :link_id
+  attr_accessible :user_id, :link_id, :avatar
   validates_uniqueness_of :user_id, :scope => [:link_id]
   
   belongs_to :user
